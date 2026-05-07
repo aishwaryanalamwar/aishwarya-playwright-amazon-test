5400007ee82eaafa2f04841d68661c7ae832469b
Aishwarya Nalamwar <aishwaryanalamwar1503@gmail.com>
2026-05-07 18:03:20 +0530

    Remove test.only from Screenshots spec

--- tests/Screenshots.spec.js
+++ tests/Screenshots.spec.js
@@ -5,13 +5,13 @@ const {test, expect} = require('@playwright/test')
     await page.screenshot({path: 'tests/Screenshots/' + Date.now() + '_Homepage.png'})
 });
 
-    test.only('Screenshots full page', async ({page}) => {
+    test('Screenshots full page', async ({page}) => {
     await page.goto('https://demo.nopcommerce.com/')
     await page.screenshot({path: 'tests/Screenshots/' + Date.now() + 'Fullpage.png', fullPage:true})
 
 });
 
-test.only('Screenshots Element', async ({page}) => {
+test('Screenshots Element', async ({page}) => {
     await page.goto('https://demo.nopcommerce.com/')
     await page.locator("//img[@title='Show details for Apple MacBook Pro']").screenshot({path: 'tests/Screenshots/' + Date.now() + 'macbook.png'})
 
